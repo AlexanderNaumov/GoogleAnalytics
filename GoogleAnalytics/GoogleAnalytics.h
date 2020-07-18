d7dacf5156ae44f23b549a9f4e3823b7e3cf6583
Alexander <haymob@me.xom>
2020-07-18 15:04:47 +0300

    init

---
+++ GoogleAnalytics/GoogleAnalytics.h
@@ -0,0 +1,20 @@
+//
+//  GoogleAnalytics.h
+//  GoogleAnalytics
+//
+//  Created by Alexander Naumov on 18.07.2020.
+//  Copyright © 2020 Alexander Naumov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "GAI.h"
+#import "GAIDictionaryBuilder.h"
+#import "GAIEcommerceFields.h"
+#import "GAIEcommerceProduct.h"
+#import "GAIEcommerceProductAction.h"
+#import "GAIEcommercePromotion.h"
+#import "GAIFields.h"
+#import "GAILogger.h"
+#import "GAITrackedViewController.h"
+#import "GAITracker.h"
